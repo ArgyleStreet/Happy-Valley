08c5e60112c51a2117457c87b19575fb39d048d2
samson-sham <samson-sham>
2017-01-20 10:05:47 +0900

    [#3] Added: static foreground

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ let game = new Phaser.Game(800, 600, Phaser.WEBGL_MULTI, 'happy-valley', {
 function preload() {
     game.load.image(IMG_SKY, 'resources/sky.jpg');
     game.load.image(IMG_TERRAIN, 'resources/terrain.png');
+    game.load.image(IMG_BUSH1, 'resources/bush1.png');
+    game.load.image(IMG_BUSH2, 'resources/bush2.png');
+    game.load.image(IMG_BUSH3, 'resources/bush3.png');
     game.load.spritesheet(IMG_HORSE, 'resources/sprite-horse.png', 732/4, 536/4);
 }
 function create() {
@@ -24,7 +27,7 @@ function create() {
     let scale = 0.2,
         x = 0,
         y = -20,
-        width = 800 / scale,
+        width = game.world.width / scale,
         height = 140;
     let sky = game.add.tileSprite(x, y, width, height, IMG_SKY);
     sky.tileScale.set(scale,scale);
@@ -33,6 +36,30 @@ function create() {
     // ground 
     // @TODO dynamic population of tiles
     game.add.tileSprite(x, skyBottom, game.world.width, game.world.height-skyBottom, IMG_TERRAIN);
+
+    // bushes
+    // @TODO dynamic population of tiles
+    // @TODO layers of foreground
+    let image, sprite;
+    let bushLayer1 = game.add.group(),
+        bushLayer2 = game.add.group();
+    scale = 0.2;
+    for (let i=0;i<game.rnd.between(5,12);i++) {
+        image = game.rnd.pick([IMG_BUSH1, IMG_BUSH2, IMG_BUSH3]);
+        sprite = game.add.sprite(game.world.randomX/scale, (skyBottom+5)/scale, image);
+        sprite.anchor.set(0.5, 1);
+        bushLayer1.add(sprite);
+    }
+    bushLayer1.scale.set(scale, scale);
+    scale = 0.4;
+    for (let i=0;i<game.rnd.between(2,7);i++) {
+        image = game.rnd.pick([IMG_BUSH1, IMG_BUSH2, IMG_BUSH3]);
+        sprite = game.add.sprite(game.world.randomX/scale, (skyBottom+10)/scale, image);
+        sprite.anchor.set(0.5, 1);
+        bushLayer2.add(sprite);
+    }
+    bushLayer2.scale.set(scale, scale);
+
     // @TODO camera
 
     let horse = game.add.sprite(300, 300, IMG_HORSE);
